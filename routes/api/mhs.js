ace43a4c87c5a18da1258373166e6fe277070911
fahrijal-alkuja <rizal.alkuja@gmal.com>
2020-12-30 18:33:15 +0800

    menambahkan endpoin mhs

--- routes/api/mhs.js
+++ routes/api/mhs.js
@@ -15,4 +15,41 @@ router.post("/add", (req, res) => {
     });
   });
 });
+
+//!fungsi GET
+router.get("/dataMhs", function (req, res) {
+  // Mhs.find(function (err, mhs) {
+  //   if (err) return next(err);
+  //   res.json(mhs);
+  // });
+  Mhs.find().then(mhs => {
+    return res.status(200).json({
+      success: true,
+      msg: "Data all mhs.",
+      data: res.json(mhs)
+    });
+  });
+});
+
+//!delete function
+router.delete("/delete/:_id", (req, res) => {
+  Mhs.findByIdAndRemove({ _id: req.params._id }).then(mhs => {
+    return res.status(200).json({
+      success: true,
+      msg: "Data Berhasil Dihapus."
+    });
+  });
+});
+
+//!update function
+router.put("/update/:_id", function (req, res, next) {
+  Mhs.findByIdAndUpdate(req.params._id, req.body, function (err) {
+    if (err) return next(err);
+    res.status(200).json({
+      success: true,
+      msg: "Data Berhasil Diupdate."
+    });
+  });
+});
+
 module.exports = router;
