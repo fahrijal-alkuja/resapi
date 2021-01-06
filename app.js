c440a9069d1c726d7039a223b732f95c50af388b
fahrijal-alkuja <rizal.alkuja@gmal.com>
2021-01-06 09:56:48 +0800

    menambah configurasi server

--- app.js
+++ app.js
@@ -56,9 +56,4 @@ app.use("/api/mhs", mhs);
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public/index.html"));
 // });
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+module.exports = app;
